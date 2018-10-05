ec509c8a348f8133a9e7fb83076ed6ef34828ba1
Nolin7777 <Nolin7777@users.noreply.github.com>
2018-10-05 02:54:03 +0200

    Implement UndermapRecall

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -2303,6 +2303,36 @@ class TC_GAME_API Player : public Unit, public GridObject<Player>
         void SendCinematicStart(uint32 CinematicSequenceId) const;
         void SendMovieStart(uint32 MovieId) const;
 
+        // Anti Undermap
+        void SaveNoUndermapPosition(float x, float y, float z)
+        {
+            _lastSafeX = x;
+            _lastSafeY = y;
+            _lastSafeZ = z;
+            _undermapPosValid = true;
+        }
+
+        bool UndermapRecall()
+        {
+            if (!_undermapPosValid || IsBeingTeleported())
+                return false;
+
+            if (GetDistance2d(_lastSafeX, _lastSafeY) > 100.0f)
+            {
+                _undermapPosValid = false;
+                return false;
+            }
+
+            NearTeleportTo(_lastSafeX, _lastSafeY, _lastSafeZ + 1.5f, GetOrientation(), TELE_TO_NOT_LEAVE_COMBAT | TELE_TO_NOT_UNSUMMON_PET);
+            _undermapPosValid = false;
+            return true;
+        }
+
+        float _lastSafeX;
+        float _lastSafeY;
+        float _lastSafeZ;
+        bool _undermapPosValid;
+
         /*********************************************************/
         /***                 INSTANCE SYSTEM                   ***/
         /*********************************************************/
