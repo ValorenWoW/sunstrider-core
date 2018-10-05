5a40bf02d2c9b28cfdff69f33daec84920571e8c
Nolin7777 <Nolin7777@users.noreply.github.com>
2018-10-05 02:05:34 +0200

    Update spawn orentation

--- sql/updates/world/world_2018_10_05_01.sql
+++ sql/updates/world/world_2018_10_05_01.sql
@@ -3,13 +3,13 @@
 ALTER TABLE `playercreateinfo` ADD `position_o` FLOAT NOT NULL AFTER `position_z`;
 
 -- Fill `position_o` column with data
-UPDATE `playercreateinfo` SET `position_o` = 6.02937 WHERE `race` = 1;  -- Human
-UPDATE `playercreateinfo` SET `position_o` = 6.00392 WHERE `race` = 2;  -- Orc
-UPDATE `playercreateinfo` SET `position_o` = 6.23886 WHERE `race` = 3;  -- Dwarf
-UPDATE `playercreateinfo` SET `position_o` = 6.02401 WHERE `race` = 4;  -- Nightelf
-UPDATE `playercreateinfo` SET `position_o` = 3.07090 WHERE `race` = 5;  -- Undead
-UPDATE `playercreateinfo` SET `position_o` = 3.08639 WHERE `race` = 6;  -- Tauren
-UPDATE `playercreateinfo` SET `position_o` = 6.23886 WHERE `race` = 7;  -- Gnome
-UPDATE `playercreateinfo` SET `position_o` = 6.00392 WHERE `race` = 8;  -- Troll
-UPDATE `playercreateinfo` SET `position_o` = 5.22683 WHERE `race` = 10; -- Bloodelf
-UPDATE `playercreateinfo` SET `position_o` = 6.04596 WHERE `race` = 11; -- Draenei
+UPDATE `playercreateinfo` SET `position_o` = 6.02930 WHERE `race` = 1;  -- Human
+UPDATE `playercreateinfo` SET `position_o` = 0.02266 WHERE `race` = 2;  -- Orc
+UPDATE `playercreateinfo` SET `position_o` = 6.17184 WHERE `race` = 3;  -- Dwarf
+UPDATE `playercreateinfo` SET `position_o` = 5.92975 WHERE `race` = 4;  -- Nightelf
+UPDATE `playercreateinfo` SET `position_o` = 3.12587 WHERE `race` = 5;  -- Undead
+UPDATE `playercreateinfo` SET `position_o` = 0.09402 WHERE `race` = 6;  -- Tauren
+UPDATE `playercreateinfo` SET `position_o` = 6.17184 WHERE `race` = 7;  -- Gnome
+UPDATE `playercreateinfo` SET `position_o` = 0.02266 WHERE `race` = 8;  -- Troll
+UPDATE `playercreateinfo` SET `position_o` = 5.32500 WHERE `race` = 10; -- Bloodelf
+UPDATE `playercreateinfo` SET `position_o` = 2.00669 WHERE `race` = 11; -- Draenei
